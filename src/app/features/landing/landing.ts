38e71ba746cf7545526af59a0d4e7c4ab1c69a28
ahmeddabbas19 <ahmeddabbas19@gmail.com>
2026-06-08 12:45:43 +0300

    feat: add institute section

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -6,11 +6,12 @@ import { Values } from './values/values';
 import { Join } from "./join/join";
 import { Community } from './community/community';
 import { Advantages } from './advantages/advantages';
+import { Institute } from './institute/institute';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.html',
-  imports: [Hero, About, Programs, Values, Community, Advantages, Join]
+  imports: [Hero, About, Programs, Values, Community, Institute, Advantages, Join]
 })
 export class Landing {
   
