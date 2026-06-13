853e67caf6dc018f2990c7033000e9121fe179b5
ahmeddabbas19 <ahmeddabbas19@gmail.com>
2026-06-13 17:53:51 +0300

    feat: ✨ add institute pages

--- src/app/features/landing/institute/institute.ts
+++ src/app/features/landing/institute/institute.ts
@@ -1,15 +1,12 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-institute',
-  imports: [NgStyle],
+  imports: [NgStyle, RouterLink],
   templateUrl: './institute.html'
 })
 export class Institute {
   list = ["Project-Based Learning", "Practical Tools & Templates", "Certificate of Completion", "DTL Community Access"];
-
-  exploreInstitute() {
-    window.open('https://lms.digitaltwinleaders.com/', '_blank');
-  }
 }
