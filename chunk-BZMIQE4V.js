04a654c77a1ac91e9c81c7793ab8c5b27cc5f702
ahmeddabbas19 <ahmeddabbas19@gmail.com>
2026-07-18 15:32:29 +0300

    Auto-generated commit

---
+++ chunk-BZMIQE4V.js
@@ -0,0 +1,1 @@
+var e={production:!0,firebase:{apiKey:"AIzaSyBZm330nB9225eKJuJvC9jUEllwJDHN13c",authDomain:"digitaltwinleaders-233df.firebaseapp.com",projectId:"digitaltwinleaders-233df",storageBucket:"digitaltwinleaders-233df.firebasestorage.app",messagingSenderId:"1001706627486",appId:"1:1001706627486:web:8c29d43752ce5bd446f68f"}};export{e as a};
